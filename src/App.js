84ff7d07f5cffaf6cc5c693b83133a5057bf349c
MahmoudDayeemplus <mahmoud.dayeem+@gmail.com>
2025-10-15 21:36:29 +0300

    progress

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const apiUrl = process.env.REACT_APP_API_URL;
+
+  console.log(apiUrl)
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +18,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+{         apiUrl
+}
         </a>
       </header>
     </div>
